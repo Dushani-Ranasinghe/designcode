c31cd28287945d927ad2605886b3a257e3b9f545
Dushani-Ranasinghe <uththaradushani@gmail.com>
2023-02-06 11:36:58 +0530

    Created recent course card list

--- lib/main.dart
+++ lib/main.dart
@@ -25,13 +25,91 @@ class MyApp extends StatelessWidget {
           child: Column(
             children: [
               HomeScreenNavBar(),
-              RecentCourceCard(
-                course: recentCourses[0],
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      "Recents",
+                      style: kLargeTitleStyle,
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Text(
+                      "23 courses, more coming",
+                      style: kSubtitleStyle,
+                    )
+                  ],
+                ),
               ),
+              SizedBox(height: 20.0,),
+              RecentCourseList(),
             ],
           ),
         ),
       )),
+    );
+  }
+}
+
+class RecentCourseList extends StatefulWidget {
+  const RecentCourseList({super.key});
+
+  @override
+  State<RecentCourseList> createState() => _RecentCourseListState();
+}
+
+class _RecentCourseListState extends State<RecentCourseList> {
+  List<Container> indicators = [];
+  int currentPage = 0;
+
+  Widget updateIndicators() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: recentCourses.map((course) {
+        var index = recentCourses.indexOf(course);
+        return Container(
+          width: 7.0,
+          height: 7.0,
+          margin: EdgeInsets.symmetric(horizontal: 6.0),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: currentPage == index ? Color(0xFF0971FE) : Color(0xFFA6AEBD),
+          ),
+        );
+      }).toList(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 320,
+          width: double.infinity,
+          child: PageView.builder(
+            itemBuilder: (context, index) {
+              return Opacity(
+                opacity: currentPage == index ? 1.0 : 0.5,
+                child: RecentCourceCard(
+                  course: recentCourses[index],
+                ),
+              );
+            },
+            itemCount: recentCourses.length,
+            controller: PageController(initialPage: 0, viewportFraction: 0.63),
+            onPageChanged: (index){
+              setState(() {
+                currentPage = index;
+              });
+            },
+          ),
+        ),
+        updateIndicators(),
+      ],
     );
   }
 }
@@ -52,7 +130,9 @@ class HomeScreenNavBar extends StatelessWidget {
             Icons.notifications,
             color: kPrimaryLabelColor,
           ),
-          SizedBox(width: 16.0,),
+          SizedBox(
+            width: 16.0,
+          ),
           CircleAvatar(
             radius: 18.0,
             backgroundImage: AssetImage('asset/images/profile.jpg'),
@@ -70,40 +150,39 @@ class SearchFieldWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
         child: Padding(
-          padding: EdgeInsets.only(left: 12.0, right: 33.0),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(14.0),
-              boxShadow: [
-                BoxShadow(
-                  color: kShadowColor,
-                  offset: Offset(0,12),
-                  blurRadius: 16.0,
-                )
-              ]
-            ),
-      child: Padding(
-          padding: EdgeInsets.symmetric(horizontal:16.0 ),
+      padding: EdgeInsets.only(left: 12.0, right: 33.0),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(14.0),
+            boxShadow: [
+              BoxShadow(
+                color: kShadowColor,
+                offset: Offset(0, 12),
+                blurRadius: 16.0,
+              )
+            ]),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
           child: TextField(
             cursorColor: kPrimaryLabelColor,
             decoration: InputDecoration(
-              icon: Icon(
-                Icons.search,
-                color: kPrimaryLabelColor,
-                size: 20.0,
-              ),
-            border: InputBorder.none,
-            hintText: "Serch for Courses",
-            hintStyle: kSearchPlaceholderStyle),
+                icon: Icon(
+                  Icons.search,
+                  color: kPrimaryLabelColor,
+                  size: 20.0,
+                ),
+                border: InputBorder.none,
+                hintText: "Serch for Courses",
+                hintStyle: kSearchPlaceholderStyle),
             style: kSearchTextStyle,
-            onChanged: (newText){
+            onChanged: (newText) {
               print(newText);
             },
           ),
+        ),
       ),
-    ),
-        ));
+    ));
   }
 }
 
