7ae46ff5b5cdae95056d47b4fc67e21a59533e88
Dushani-Ranasinghe <uththaradushani@gmail.com>
2023-02-06 22:28:42 +0530

    Added content to sliding up panel

--- lib/screens/continue_watching_screen.dart
+++ lib/screens/continue_watching_screen.dart
@@ -19,7 +19,38 @@ class ContinueWatchingScreen extends StatelessWidget {
       ],
       minHeight: 85.0,
       maxHeight: MediaQuery.of(context).size.height * 0.75,
-      panel: Center(child: Text("This is the SlidingUpPanel")),
+      panel: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Padding(
+              padding: EdgeInsets.only(top: 12.0, bottom: 16.0),
+              child: Container(
+                width: 42.0,
+                height: 4.0,
+                decoration: BoxDecoration(
+                  color: Color(0xFFC5CBD6),
+                  borderRadius: BorderRadius.circular(2.0),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 32.0),
+            child: Text(
+              "Continue Watching",
+              style: kTitle2Style,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 32.0),
+            child: Text(
+              "Certificates",
+              style: kTitle2Style,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
