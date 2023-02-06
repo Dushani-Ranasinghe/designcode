fbdea8c7eaa68a47f6b28d23541d7ece7c7ebd12
Dushani-Ranasinghe <uththaradushani@gmail.com>
2023-02-06 11:41:09 +0530

    Removed course list from main

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:designcode/model/course.dart';
 import 'package:flutter/material.dart';
 
 import 'components/cards/recent_course_card.dart';
+import 'components/lists/recent_course_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -54,65 +55,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class RecentCourseList extends StatefulWidget {
-  const RecentCourseList({super.key});
-
-  @override
-  State<RecentCourseList> createState() => _RecentCourseListState();
-}
-
-class _RecentCourseListState extends State<RecentCourseList> {
-  List<Container> indicators = [];
-  int currentPage = 0;
-
-  Widget updateIndicators() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: recentCourses.map((course) {
-        var index = recentCourses.indexOf(course);
-        return Container(
-          width: 7.0,
-          height: 7.0,
-          margin: EdgeInsets.symmetric(horizontal: 6.0),
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: currentPage == index ? Color(0xFF0971FE) : Color(0xFFA6AEBD),
-          ),
-        );
-      }).toList(),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: 320,
-          width: double.infinity,
-          child: PageView.builder(
-            itemBuilder: (context, index) {
-              return Opacity(
-                opacity: currentPage == index ? 1.0 : 0.5,
-                child: RecentCourceCard(
-                  course: recentCourses[index],
-                ),
-              );
-            },
-            itemCount: recentCourses.length,
-            controller: PageController(initialPage: 0, viewportFraction: 0.63),
-            onPageChanged: (index){
-              setState(() {
-                currentPage = index;
-              });
-            },
-          ),
-        ),
-        updateIndicators(),
-      ],
-    );
-  }
-}
 
 class HomeScreenNavBar extends StatelessWidget {
   const HomeScreenNavBar({super.key});
