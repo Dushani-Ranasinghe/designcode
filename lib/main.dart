1c5e5684d2ba5d510aec0965c0e81bd1f89907a1
Dushani-Ranasinghe <uththaradushani@gmail.com>
2023-02-06 09:42:12 +0530

    Created home nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 // import 'package:designcode/screens/sidebar_screen.dart';
 
+import 'package:designcode/constants.dart';
 import 'package:designcode/model/course.dart';
 import 'package:flutter/material.dart';
 
@@ -18,11 +19,51 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          body: SafeArea(
-              child: RecentCourceCard(
-        course: recentCourses[0],
-      ))),
+          body: Container(
+        color: kBackgroundColor,
+        child: SafeArea(
+          child: Column(
+            children: [
+              HomeScreenNavBar(),
+              RecentCourceCard(
+                course: recentCourses[0],
+              ),
+            ],
+          ),
+        ),
+      )),
     );
   }
 }
 
+class HomeScreenNavBar extends StatelessWidget {
+  const HomeScreenNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        RawMaterialButton(
+          onPressed: () => {},
+          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          highlightColor: Colors.transparent,
+          splashColor: Colors.transparent,
+          constraints: BoxConstraints(maxHeight: 40.0, maxWidth: 40.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(14.0),
+              boxShadow: [
+                BoxShadow(
+                  color: kShadowColor,
+                  offset: Offset(const 0,12),
+                  blurRadius: 16.0
+                )
+              ]
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
