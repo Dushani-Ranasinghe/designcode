7f7d0b05aaa8b29a246893c513970e272bbd8f52
Dushani-Ranasinghe <uththaradushani@gmail.com>
2023-02-06 10:35:06 +0530

    Created top navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -41,14 +41,36 @@ class HomeScreenNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        RawMaterialButton(
-          onPressed: () => {},
-          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-          highlightColor: Colors.transparent,
-          splashColor: Colors.transparent,
-          constraints: BoxConstraints(maxHeight: 40.0, maxWidth: 40.0),
+    return Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SideBarButton(),
+          SearchFieldWidget(),
+          Icon(
+            Icons.notifications,
+            color: kPrimaryLabelColor,
+          ),
+          SizedBox(width: 16.0,),
+          CircleAvatar(
+            radius: 18.0,
+            backgroundImage: AssetImage('asset/images/profile.jpg'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SearchFieldWidget extends StatelessWidget {
+  const SearchFieldWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Padding(
+          padding: EdgeInsets.only(left: 12.0, right: 33.0),
           child: Container(
             decoration: BoxDecoration(
               color: Colors.white,
@@ -56,14 +78,62 @@ class HomeScreenNavBar extends StatelessWidget {
               boxShadow: [
                 BoxShadow(
                   color: kShadowColor,
-                  offset: Offset(const 0,12),
-                  blurRadius: 16.0
+                  offset: Offset(0,12),
+                  blurRadius: 16.0,
                 )
               ]
             ),
+      child: Padding(
+          padding: EdgeInsets.symmetric(horizontal:16.0 ),
+          child: TextField(
+            cursorColor: kPrimaryLabelColor,
+            decoration: InputDecoration(
+              icon: Icon(
+                Icons.search,
+                color: kPrimaryLabelColor,
+                size: 20.0,
+              ),
+            border: InputBorder.none,
+            hintText: "Serch for Courses",
+            hintStyle: kSearchPlaceholderStyle),
+            style: kSearchTextStyle,
+            onChanged: (newText){
+              print(newText);
+            },
           ),
-        )
-      ],
+      ),
+    ),
+        ));
+  }
+}
+
+class SideBarButton extends StatelessWidget {
+  const SideBarButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: () => {print('sidebar button')},
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      highlightColor: Colors.transparent,
+      splashColor: Colors.transparent,
+      constraints: BoxConstraints(maxHeight: 40.0, maxWidth: 40.0),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(14.0),
+            boxShadow: [
+              BoxShadow(
+                  color: kShadowColor, offset: Offset(0, 12), blurRadius: 16.0)
+            ]),
+        padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 14.0),
+        child: Image.asset(
+          'asset/icons/icon-sidebar.png',
+          color: kPrimaryLabelColor,
+        ),
+      ),
     );
   }
 }
