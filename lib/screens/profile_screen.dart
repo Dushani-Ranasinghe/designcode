4869ca8eb9d35b1ac47dfd3a15318e4ca477ca34
Dushani-Ranasinghe <uththaradushani@gmail.com>
2023-02-19 14:12:17 +0530

    created back button for profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,10 +1,77 @@
 import 'package:flutter/material.dart';
 
+import '../constants.dart';
+
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: kCardPopupBackgroundColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(34.0),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: kShadowColor,
+                    offset: Offset(0, 12),
+                    blurRadius: 32.0,
+                  ),
+                ],
+              ),
+              child: SafeArea(
+                bottom: false,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: 24.0,
+                        bottom: 32.0,
+                        left: 20.0,
+                        right: 20.0,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          RawMaterialButton(
+                            materialTapTargetSize:
+                                MaterialTapTargetSize.shrinkWrap,
+                            constraints: BoxConstraints(
+                                minWidth: 40.0,
+                                maxWidth: 40.0,
+                                maxHeight: 24.0),
+                            highlightColor: Colors.transparent,
+                            splashColor: Colors.transparent,
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Icon(
+                                  Icons.arrow_back,
+                                  color: kSecondaryLabelColor,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
